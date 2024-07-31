7ae88d91b2a4937fd97c15f1334876d1b630bcfe
devfxx <fxx@kty.lol>
2024-08-01 01:14:21 +0300

    refactor(src/server): The whole thing.

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,14 @@
+import dayjs from "dayjs";
+import relativeTime from "dayjs/plugin/relativeTime";
+import { Client } from "discord.js-selfbot-v13";
 import express from "express";
 import path from "path";
 import { config } from "../config";
-import { Client } from "discord.js-selfbot-v13";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime";
 
 dayjs.extend(relativeTime);
 
-let server = express();
-let client = new Client();
+const server = express();
+const client = new Client();
 
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
@@ -17,14 +17,18 @@ server.set("views", path.join(__dirname, "views"));
 
 server.get("/", async (req, res) => {
   try {
-    let friends = client.users.cache
-      .filter((user) => !user.bot)
+    const friends = client.users.cache
+      .filter((user) => {
+        if (user.bot) return false;
+        if (user.id === client.user?.id) return false;
+        return true;
+      })
       .map((user) => ({
         id: user.id,
         username: user.username,
       }));
 
-    return res.render("index.ejs", { friends });
+    return res.render("index", { friends });
   } catch (error) {
     console.error("Error fetching friends list:", error);
     return res
@@ -34,27 +38,30 @@ server.get("/", async (req, res) => {
 });
 
 server.get("/:id", async (req, res) => {
-  let { id } = req.params;
+  const { id } = req.params;
+
+  // obvious
+  if (id === "favicon.ico") return;
 
   try {
-    let user = await client.users.fetch(id);
+    const user = await client.users.fetch(id);
 
     if (!user)
       return res.status(404).json({ error: true, message: "User not found" });
 
-    let avatarUrl = user.displayAvatarURL({ format: "png", dynamic: true });
-    let dmChannel = await user.createDM();
-    let messages = await dmChannel.messages.fetch({ limit: 100 });
-    let latestMessage = messages
+    const avatarUrl = user.displayAvatarURL({ format: "png", dynamic: true });
+    const dmChannel = await user.createDM();
+    const messages = await dmChannel.messages.fetch({ limit: 100 });
+    const latestMessage = messages
       .filter((msg) => msg.author.id === user.id)
       .first();
 
-    let latestTimestamp = latestMessage ? latestMessage.createdAt : null;
-    let lastResponseTime = latestTimestamp
+    const latestTimestamp = latestMessage ? latestMessage.createdAt : null;
+    const lastResponseTime = latestTimestamp
       ? dayjs(latestTimestamp).fromNow()
       : "No messages found";
 
-    let data = {
+    const data = {
       userName: user.username,
       userId: user.id,
       lastResponseTime,
@@ -62,19 +69,19 @@ server.get("/:id", async (req, res) => {
       lastResponse: latestMessage ? latestMessage.content : "No messages found",
     };
 
-    return res.render("user.ejs", data);
+    return res.render("user", data);
   } catch (error) {
     console.error("Error fetching user details or messages:", error);
     res.status(500).json({ error: true, message: "Error fetching user data" });
   }
 });
 
-server.listen(3000, () => {
-  console.log("Web is running on port 3000");
-});
-
 client.on("ready", (client) => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.login(config.userToken);
+await client.login(config.userToken);
+
+server.listen(config.port, () => {
+  console.log(`Web is running on port ${config.port}`);
+});
